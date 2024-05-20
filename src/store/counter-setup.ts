bd5ffbf77e6931de848ff247fc08d4a24c84d184
Desarrollos Web Urquiza <desarrollos_web_urquiza@hotmail.com>
2024-05-19 23:22:16 -0300

    using Pinia with Options and Setup

---
+++ src/store/counter-setup.ts
@@ -0,0 +1,30 @@
+import { ref } from "vue";
+import { defineStore } from "pinia";
+
+interface CounterOptionsState {
+    count: number;
+    lastChanged?: Date;
+}
+
+export const useCounterOptionsStore = defineStore('counterSetup', () => {
+    const count = ref<number>(0);
+    const lastChanged = ref<Date>();
+
+    const incrementBy = (value: number) => {
+        count.value += value;
+        lastChanged.value = new Date();
+    }
+
+    return {
+        //State properties
+        count,
+        lastChanged,
+
+        //Getters
+        squareCount: computed(() => count.value * count.value),
+
+        //Actions
+        incrementBy,
+        increment: () => incrementBy(1),
+    }
+});
