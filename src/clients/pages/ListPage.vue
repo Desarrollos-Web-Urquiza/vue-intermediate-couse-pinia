9e61f2f88c8d3b65977e13e9f1fdb223ceba821b
Desarrollos Web Urquiza <desarrollos_web_urquiza@hotmail.com>
2024-05-22 23:16:56 -0300

    creating the clients section

---
+++ src/clients/pages/ListPage.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+
+<template>
+    <div>
+        <h1>Listado de clientes</h1>
+        <RouterView />
+    </div>
+</template>
+
+<style scoped>
+
+</style>
