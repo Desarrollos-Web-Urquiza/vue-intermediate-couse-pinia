096b9ba24db1bf9a5f7a11f0c6cfe503704603f7
Desarrollos Web Urquiza <desarrollos_web_urquiza@hotmail.com>
2024-06-02 23:48:15 -0300

    creating the Clients by ID link

--- src/clients/pages/ListPage.vue
+++ src/clients/pages/ListPage.vue
@@ -1,4 +1,5 @@
 <script setup lang="ts">
+import ClientList from '@/clients/components/ClientList.vue';
 
 </script>
 
@@ -6,6 +7,14 @@
 <template>
     <div>
         <h1>Listado de clientes</h1>
+
+        <!-- Client List --> 
+        <ClientList /> 
+
+        <!-- PaginationNumbers --> 
+
+        <!-- LoadingModal --> 
+
         <RouterView />
     </div>
 </template>
