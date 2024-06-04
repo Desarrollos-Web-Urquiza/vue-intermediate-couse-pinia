c53d5417c19a0db3cb70ceb02a5721afd3d63507
Desarrollos Web Urquiza <desarrollos_web_urquiza@hotmail.com>
2024-06-04 20:32:13 -0300

    using '@tanstack/vue-query' in the project

---
+++ src/clients/composables/useClients.ts
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/vue-query';
+import clientsApi from '@/api/clients-api';
+import type { Client } from '@/clients/interfaces/client';
+
+const getClients = async():Promise<Client[]> => {
+    const { data } = await clientsApi.get()<Client[]>('/clients?_page=1');
+    return data;
+}
+
+const useClients = () => {
+
+    const { isLoading, data } = useQuery(
+        ['clients?_page=', 1],
+        () => getClients()
+    )
+
+    return {
+
+    }
+
+}
+
+export default useClients;
