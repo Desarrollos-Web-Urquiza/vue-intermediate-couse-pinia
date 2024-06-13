0a3459e7b0d1f847aed1cb9265ad9d7dba569376
Desarrollos Web Urquiza <desarrollos_web_urquiza@hotmail.com>
2024-06-12 23:50:00 -0300

    adding a loading to pagination

--- src/clients/composables/useClients.ts
+++ src/clients/composables/useClients.ts
@@ -6,6 +6,11 @@ import { watch, computed } from 'vue';
 import type { Client } from '@/clients/interfaces/client';
 
 const getClients = async( page:number ):Promise<Client[]> => {
+
+    // await new Promise ( resolve =>  {
+    //     setTimeout(() => resolve(true), 1500 );
+    // })
+
     const { data } = await clientsApi.get<Client[]>(`/clients?_page=${ page }`);
     return data;
 }
@@ -18,6 +23,9 @@ const useClients = () => {
     const { isLoading, data } = useQuery(
         ['clients?_page=', currentPage],
         () => getClients( currentPage.value ),
+        {
+            // staleTime: 1000 * 60,
+        }
     )
 
     watch( data, clients =>  {
--- src/clients/pages/ListPage.vue
+++ src/clients/pages/ListPage.vue
@@ -1,6 +1,10 @@
 <script setup lang="ts">
 import ClientList from '@/clients/components/ClientList.vue';
 import PaginationNumbers from '@/clients/components/PaginationNumbers.vue';
+import useClients from '../composables/useClients';
+import LoadingModal from '@/shared/components/icons/LoadingModal.vue';
+
+const { isLoading } = useClients();
 
 </script>
 
@@ -8,7 +12,8 @@ import PaginationNumbers from '@/clients/components/PaginationNumbers.vue';
 <template>
     <div>
         <h1>Listado de clientes</h1>
-
+        
+        <LoadingModal v-if="isLoading" />
         <!-- Client List --> 
         <ClientList /> 
 
@@ -17,6 +22,7 @@ import PaginationNumbers from '@/clients/components/PaginationNumbers.vue';
         <!-- LoadingModal --> 
 
         <PaginationNumbers />
+
     </div>
 </template>
 
